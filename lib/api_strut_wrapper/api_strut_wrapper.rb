77d630590826f20b5147baa99738f6bf6f5887b6
kanechika <kanechika7@gmail.com>
2011-12-07 13:31:10 +0900

    add strut_wrapper

--- lib/api_strut_wrapper/api_strut_wrapper.rb
+++ lib/api_strut_wrapper/api_strut_wrapper.rb
@@ -1,6 +1,7 @@
 # coding: UTF-8
 module ApiStrutWrapper
   require File.join(File.dirname(__FILE__), 'api_strut_wrapper/document')
+  require File.join(File.dirname(__FILE__), 'api_strut_wrapper/strut_wrapper')
   class ApiStrutWrapper::Error < StandardError; end
 end
 
